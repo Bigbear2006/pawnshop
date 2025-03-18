ff2102efe906ca823919edb860386c9dbb1ad207
Bigbear2006 <morozm982@gmail.com>
2025-03-18 18:11:09 +0300

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+
+import django
+from aiogram import F
+from aiogram.types import BotCommand
+
+from bot.loader import bot, dp, logger
+
+
+async def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    django.setup()
+
+    from bot.handlers import commands
+
+    dp.include_routers(
+        commands.router,
+    )
+    dp.message.filter(F.chat.type == 'private')
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(
+        [
+            BotCommand(command='/start', description='Запустить бота'),
+        ],
+    )
+
+    logger.info('Starting bot...')
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
